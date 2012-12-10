40c008b2e514c0dd6bfb5015c0f7ec58c1b1a182
Drakosh <cpsoft@gmail.com>
2012-12-11 01:22:53 +0400

    cleanup and refactor

--- IncubatorStat.py
+++ IncubatorStat.py
@@ -4,17 +4,26 @@
 Автор: http://ru.wikipedia.org/wiki/Участник:Drakosh
 Лицензирование: GNU GPL v3 / Beerware."""
 
-import httplib, urllib, wikipedia, catlib, locale
+import wikipedia, catlib
 
 class IncubatorException(Exception):
+    """just an exception"""
     pass
 
 class WikiInfo:
+    """Info about a wiki in incubator"""
     def __init__(self, cat):
         self.cat = cat
-        self.localname= None
+        self.articles = len(self.cat.articlesList(recurse = 3))
+        self.pages = 0
+        self.redir = 0
+        self.users = 0
+        self.edits = 0
+        self.littleedits = 0
+        
         if self.cat.titleWithoutNamespace()[:2] == u"Wp":
             self.lang = self.cat.titleWithoutNamespace()[3:].encode("utf-8")
+            self.localname = self.lang
         else:
             raise IncubatorException
         for tl in self.cat.templatesWithParams():
@@ -27,15 +36,18 @@ class WikiInfo:
                         break
 
     def __repr__(self):
-        return "%s %s" % (self.lang, self.localname)
+        return "Lang:%s\tLocalname:%s\tA:%s\tP:%s\tR:%s\tU:%s" % (self.lang, self.localname, self.articles, self.pages, self.redir, self.users)
         #return self.lang + self.localname
 
 if __name__ == "__main__":
+    f = open('/home/drakosh/IncubatorStat.txt', 'w+')    
     site = wikipedia.getSite('incubator', 'incubator')
-    cat = catlib.Category(site, u"Category:Incubator:All_test_wikis")
-    for c in cat.subcategories():
+    for c in catlib.Category(site, u"Category:Incubator:All_test_wikis").subcategories():
         try:
             i = WikiInfo(c)
+            f.write("%s\r\n" % i)
+            f.flush()
             print i
         except IncubatorException:
             pass
+    f.close()
