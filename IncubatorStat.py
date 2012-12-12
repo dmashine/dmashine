1a2d5bb4e93095131e27c83027ffaef893a23d1e
Drakosh <cpsoft@gmail.com>
2012-12-12 22:06:22 +0400

    now it works a little

--- IncubatorStat.py
+++ IncubatorStat.py
@@ -4,7 +4,8 @@
 Автор: http://ru.wikipedia.org/wiki/Участник:Drakosh
 Лицензирование: GNU GPL v3 / Beerware."""
 
-import wikipedia, catlib
+import wikipedia, catlib, re
+from httphelp import httphelp
 
 class IncubatorException(Exception):
     """just an exception"""
@@ -14,12 +15,12 @@ class WikiInfo:
     """Info about a wiki in incubator"""
     def __init__(self, cat):
         self.cat = cat
-        self.articles = len(self.cat.articlesList(recurse = 3))
+        self.articles = 0# len(self.cat.articlesList(recurse = 3))
         self.pages = 0
         self.redir = 0
-        self.users = 0
+        self.editors = 0
         self.edits = 0
-        self.littleedits = 0
+        self.littleedits = 0       
         
         if self.cat.titleWithoutNamespace()[:2] == u"Wp":
             self.lang = self.cat.titleWithoutNamespace()[3:].encode("utf-8")
@@ -34,9 +35,28 @@ class WikiInfo:
                         self.localname =  param[param.find(u"=")+1:].strip().encode("utf-8")
                         # i fucking LOVE unicode
                         break
-
+        self.conn = httphelp()
+        self.conn.server     = "toolserver.org"
+        self.conn.scriptname = "/~pathoschild/catanalysis/index.php?title=Wp/%s&wiki=incubatorwiki_p&cat=0" % self.lang
+        self.conn.parameters = {}
+        self.conn.codepage = "utf-8"
+        self.lines = self.conn.lines("GET")
+        for l in self.lines:
+            m = re.search(u'<li>(\d*) articles', l)
+            if m != None:
+                self.pages = m.group(1).encode("utf-8")
+            m = re.search(u'<li>(\d*) editors', l)
+            if m != None:
+                self.editors= m.group(1).encode("utf-8")
+            m = re.search(u'<li>(\d*) revisions', l)
+            if m != None:
+                self.edits= m.group(1).encode("utf-8")
+            m = re.search(u'including (\d*) minor', l)
+            if m != None:
+                self.littleedits = m.group(1).encode("utf-8")
+        
     def __repr__(self):
-        return "Lang:%s\tLocalname:%s\tA:%s\tP:%s\tR:%s\tU:%s" % (self.lang, self.localname, self.articles, self.pages, self.redir, self.users)
+        return "Lang:%s\tLocalname:%s\tA:%s\tP:%s\tR:%s\tE:%s" % (self.lang, self.localname, self.articles, self.pages, self.redir, self.editors)
         #return self.lang + self.localname
 
 if __name__ == "__main__":
@@ -45,8 +65,6 @@ if __name__ == "__main__":
     for c in catlib.Category(site, u"Category:Incubator:All_test_wikis").subcategories():
         try:
             i = WikiInfo(c)
-            f.write("%s\r\n" % i)
-            f.flush()
             print i
         except IncubatorException:
             pass
